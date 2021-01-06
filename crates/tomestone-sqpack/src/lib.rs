c707f3f5ca013a1b65baaec4f245d13e344e6ed8
David Cook <divergentdave@gmail.com>
2021-01-05 21:19:33 -0600

    Extract listing repositories

--- crates/tomestone-sqpack/src/lib.rs
+++ crates/tomestone-sqpack/src/lib.rs
@@ -1,3 +1,5 @@
+use std::{ffi::OsString, io, path::Path};
+
 pub mod parser;
 
 #[derive(Debug)]
@@ -40,4 +42,26 @@ impl SqPackType {
             _ => Err(SqPackTypeParseError),
         }
     }
+}
+
+pub fn list_repositories<P: AsRef<Path>>(root: P) -> Result<Vec<OsString>, io::Error> {
+    let sqpack_dir = root.as_ref().join("game").join("sqpack");
+    sqpack_dir
+        .read_dir()?
+        .filter_map(|res| {
+            let entry = match res {
+                Ok(entry) => entry,
+                Err(e) => return Some(Err(e)),
+            };
+            let file_type = match entry.file_type() {
+                Ok(file_type) => file_type,
+                Err(e) => return Some(Err(e)),
+            };
+            if file_type.is_dir() {
+                Some(Ok(entry.file_name()))
+            } else {
+                None
+            }
+        })
+        .collect()
 }
