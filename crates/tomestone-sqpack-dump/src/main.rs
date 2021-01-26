c91a5de59d738bdf419b21e5f6cc292e2a081fa1
David Cook <divergentdave@gmail.com>
2021-01-25 20:44:17 -0600

    Check in some more boilerplate

---
+++ crates/tomestone-sqpack-dump/src/main.rs
@@ -0,0 +1,11 @@
+use std::process;
+
+fn main() {
+    dotenv::dotenv().ok();
+    let root = if let Ok(root) = std::env::var("FFXIV_INSTALL_DIR") {
+        root
+    } else {
+        eprintln!("error: set the environment variable FFXIV_INSTALL_DIR to the game's installation directory");
+        process::exit(1);
+    };
+}
