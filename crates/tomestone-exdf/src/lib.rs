79837fecc413a84a670ed739f5a8d8922fa1872a
David Cook <divergentdave@gmail.com>
2021-02-20 15:11:14 -0600

    Initial EXHF parser

--- crates/tomestone-exdf/src/lib.rs
+++ crates/tomestone-exdf/src/lib.rs
@@ -1,1 +1,74 @@
 pub mod parser;
+
+#[derive(Debug)]
+pub struct EnumParseError;
+
+#[derive(Debug)]
+pub enum Language {
+    NoLanguage = 0,
+    Japanese = 1,
+    English = 2,
+    German = 3,
+    French = 4,
+    ChineseSingapore = 5,
+    ChineseTraditional = 6,
+    Korean = 7,
+}
+
+impl Language {
+    fn from_u16(value: u16) -> Result<Language, EnumParseError> {
+        match value {
+            0 => Ok(Language::NoLanguage),
+            1 => Ok(Language::Japanese),
+            2 => Ok(Language::English),
+            3 => Ok(Language::German),
+            4 => Ok(Language::French),
+            5 => Ok(Language::ChineseSingapore),
+            6 => Ok(Language::ChineseTraditional),
+            7 => Ok(Language::Korean),
+            _ => Err(EnumParseError),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{EnumParseError, Language};
+
+    #[test]
+    fn language_round_trip() {
+        assert!(matches!(
+            Language::from_u16(Language::NoLanguage as u16),
+            Ok(Language::NoLanguage)
+        ));
+        assert!(matches!(
+            Language::from_u16(Language::Japanese as u16),
+            Ok(Language::Japanese)
+        ));
+        assert!(matches!(
+            Language::from_u16(Language::English as u16),
+            Ok(Language::English)
+        ));
+        assert!(matches!(
+            Language::from_u16(Language::German as u16),
+            Ok(Language::German)
+        ));
+        assert!(matches!(
+            Language::from_u16(Language::French as u16),
+            Ok(Language::French)
+        ));
+        assert!(matches!(
+            Language::from_u16(Language::ChineseSingapore as u16),
+            Ok(Language::ChineseSingapore)
+        ));
+        assert!(matches!(
+            Language::from_u16(Language::ChineseTraditional as u16),
+            Ok(Language::ChineseTraditional)
+        ));
+        assert!(matches!(
+            Language::from_u16(Language::Korean as u16),
+            Ok(Language::Korean)
+        ));
+        assert!(matches!(Language::from_u16(65535), Err(EnumParseError)));
+    }
+}
--- crates/tomestone-exdf/src/parser/exhf.rs
+++ crates/tomestone-exdf/src/parser/exhf.rs
@@ -1,16 +1,100 @@
+use std::convert::TryInto;
+
 use nom::{
-    bytes::streaming::tag, combinator::map, number::streaming::le_u32, sequence::tuple, IResult,
+    bytes::streaming::tag,
+    combinator::{map, map_res},
+    multi::count,
+    number::streaming::{be_u16, be_u32, le_u16},
+    sequence::{pair, tuple},
+    IResult,
 };
 
-fn exhf_header(input: &[u8]) -> IResult<&[u8], ()> {
-    map(tuple((tag("EXHF"), le_u32, le_u32)), |_| ())(input)
+use super::null_padding;
+use crate::Language;
+
+#[derive(Debug, Clone)]
+struct ExhfHeader {
+    row_size: u16,
+    num_columns: u16,
+    num_pages: u16,
+    num_language_codes: u16,
+    num_rows: u32,
+}
+
+#[derive(Debug)]
+pub struct Exhf {
+    header: ExhfHeader,
+    column_definitions: Vec<(u16, u16)>,
+    pages: Vec<(u32, u32)>,
+    languages: Vec<Language>,
+}
+
+fn exhf_header(input: &[u8]) -> IResult<&[u8], ExhfHeader> {
+    map(
+        tuple((
+            tag("EXHF"),
+            tag("\x00\x03"),
+            be_u16, // size of dataset chunk
+            be_u16, // number of datasets
+            be_u16, // number of pages
+            be_u16, // number of lang codes
+            be_u16, // unknown
+            be_u32, // unknown
+            be_u32, // number of entries
+            null_padding(8),
+        )),
+        |(_, _, row_size, num_columns, num_pages, num_language_codes, _, _, num_rows, ())| {
+            ExhfHeader {
+                row_size,
+                num_columns,
+                num_pages,
+                num_language_codes,
+                num_rows,
+            }
+        },
+    )(input)
+}
+
+fn column_entry(input: &[u8]) -> IResult<&[u8], (u16, u16)> {
+    pair(be_u16, be_u16)(input)
+}
+
+fn page_entry(input: &[u8]) -> IResult<&[u8], (u32, u32)> {
+    pair(be_u32, be_u32)(input)
+}
+
+fn language_code(input: &[u8]) -> IResult<&[u8], Language> {
+    map_res(le_u16, Language::from_u16)(input)
+}
+
+pub fn parse_exhf(input: &[u8]) -> IResult<&[u8], Exhf> {
+    let (input, header) = exhf_header(input)?;
+    let num_columns = TryInto::<usize>::try_into(header.num_columns).unwrap();
+    let num_pages = TryInto::<usize>::try_into(header.num_pages).unwrap();
+    let num_language_codes = TryInto::<usize>::try_into(header.num_language_codes).unwrap();
+    map(
+        tuple((
+            count(column_entry, num_columns),
+            count(page_entry, num_pages),
+            count(language_code, num_language_codes),
+        )),
+        move |(column_definitions, pages, languages)| {
+            let header = header.clone();
+            Exhf {
+                header,
+                column_definitions,
+                pages,
+                languages,
+            }
+        },
+    )(input)
 }
 
 #[cfg(test)]
 mod tests {
     use tomestone_sqpack::GameData;
 
-    use super::exhf_header;
+    use super::{exhf_header, parse_exhf};
 
     #[test]
     fn exhf_game_data() {
@@ -26,5 +110,6 @@ mod tests {
 
         let exh_data = game_data.lookup_path_data(EXH_PATH).unwrap().unwrap();
         exhf_header(&exh_data).unwrap();
+        parse_exhf(&exh_data).unwrap();
     }
 }
